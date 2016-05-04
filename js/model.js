ad9890ff443aaeb058337c29ecb2ef7f57ec3ba2
Chris Ertel <chris@kedagital.com>
2016-05-04 01:00:52 -0500

    Add line parsing stubs to OBJ reader.

--- js/model.js
+++ js/model.js
@@ -5,7 +5,7 @@ function Model() {
   this._faces = new Int32Array();
 }
 
-Model.prototype.toTriArray() {
+Model.prototype.toTriArray = function() {
   // this assumes face indices are trilists and not strips
   return this._faces.reduce( function( acc, el, idx) {
     // unpack the face indexes into an array of 3-tuples
@@ -23,14 +23,81 @@ Model.prototype.toTriArray() {
   }, new Float32Array( this.faces.length * 3 * 3));
 }
 
-Model.prototype.loadFromOBJString( src ) {
+Model.prototype.loadFromOBJString = function( src ) {
   // split source into trimmed lines with comments removed
   var processedLines = src.split('\n')
                           .map( function(s) { return s.replace(/#.*/,'').trim(); })
                           .filter( function(s) { return s.length !== 0; });
 
+  var parseState = {
+    verts: [],
+    uvs: [],
+    norms: [],
+    faces: []
+  };
 
-  
+  function decodeVertex( toks ) {
+    if (toks.length < 3 || toks.length > 4) {
+      throw new Error("Malformed vertex");
+    }
+    return [];
+  };
+
+  function decodeNormal( toks ) {
+    if (toks.length !== 4) {
+      throw new Error("Malformed normal");
+    }
+    
+    return [];
+  };
+
+  function decodeUV( toks ) {
+    if (toks.length < 3 || toks.length > 4) {
+      throw new Error("Malformed UV");
+    }
+
+    return [];
+  };
+
+  function decodeFace( toks ) {
+    if (toks.length < 4) {
+      throw new Error("Malformed face");
+    }
+
+    var indices = {
+      verts: [],
+      uvs: [],
+      norms: []
+    };
+
+    return indices;
+  }
+
+  var parsedState = processedLines.reduce( function (state, line) {
+    var toks = line.split(/\s+/);
+    try {
+      switch(true) {
+        case ('v' === toks[0]) :  state.verts = state.verts.concat(decodeVertex(toks)); break;
+        case ('vn' === toks[0]) : state.norms = state.verts.concat(decodeNormal(toks)); break;
+        case ('vt' === toks[0]) : state.uvs = state.uvs.concat(decodeUV(toks)); break;
+        case ('f' === toks[0]) :  state.faces = state.faces.concat(decodeFace(toks).verts); break;
+        default: throw new Error("Unrecognized line"); break;
+      }      
+    } catch( e ) {
+      console.log("ERROR ", e.toString(), '\n>>>\t ', line);
+    }
+    return state;
+  }, parseState);
+
+  this._verts = Float32Array.from( parsedState.verts );
+  this._uvs = Float32Array.from( parsedState.uvs );
+  this._normals = Float32Array.from( parsedState.norms );
+
+  console.log('Model has\n',
+    '\t', this._verts.length/3, ' vertices\n',
+    '\t', this._uvs.length/3, ' UVs\n',
+    '\t', this._normals.length/3, ' normals\n',
+    '\t', this._faces.length/3, ' faces');
 
   return processedLines;
 }
